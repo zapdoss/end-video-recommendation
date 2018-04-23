93b5a4b74714bd4c0a6178f277f7a57edcf761ed
Kashish Jain <jainkashish96@gmail.com>
2018-04-24 03:37:33 +0530

    added script to gather information from old data

---
+++ olddatascrpt.py
@@ -0,0 +1,76 @@
+import psycopg2
+import sys
+import rule1
+import rule2
+ 
+
+usermap={}
+con = None
+ 
+try:
+    con = psycopg2.connect("host='localhost' dbname='testdb' user='pythonspot' password='password'")   
+    cur = con.cursor()
+    cur.execute("SELECT * FROM user_video ORDER BY date,time")
+ 
+    while True:
+        row = cur.fetchone()
+ 
+        if row == None:
+            break
+
+        if row[0] not in users.keys():
+            usermap[row[0]]=[]
+        usermap[row[0]].append(row[1])
+ 
+except psycopg2.DatabaseError, e:
+    if con:
+        con.rollback()
+ 
+    print 'Error %s' % e    
+    sys.exit(1)
+ 
+finally:   
+    if con:
+        con.close()
+
+
+# courses["cs1"]=[1,2,3,4,5]
+# courses["cs2"]=[6,3,8,9,10]
+# courses["cs3"]=[23,54,3,5,44]
+# courses["cs4"]=[22,3,2,44,55,66,77,88,99]
+# courses["cs5"]=[33,44,2,4,6,7]
+
+videos={}
+for k in usermap.keys():
+    for i in range(len(usermap[k])):
+        if usermap[k][i] not in videos.keys():
+            videos[usermap[k][i]]=[0,0,0]
+        mod=0
+        #course rule
+        similarVideos=getVideosR1(usermap[k][i])
+        if(i<len(usermap[k])-1):
+            if(usermap[k][i+1] in similarVideos):
+                videos[usermap[k][i]][0]=videos[usermap[k][i]][0]+1
+                mod=1
+        if(i<len(usermap[k])-2):
+            if(usermap[k][i+2] in similarVideos):
+                videos[usermap[k][i]][0]=videos[usermap[k][i]][0]+1
+                mod=1
+        #tags
+        similarVideos=getVideosR2(usermap[k][i])
+        if(i<len(usermap[k])-1):
+            if(usermap[k][i+1] in similarVideos):
+                videos[usermap[k][i]][1]=videos[usermap[k][i]][1]+1
+                mod=1
+        if(i<len(usermap[k])-2):
+            if(usermap[k][i+2] in similarVideos):
+                videos[usermap[k][i]][1]=videos[usermap[k][i]][1]+1
+                mod=1
+        #similarusers
+        if mod==0:
+            videos[usermap[k][i]][2]=videos[usermap[k][i]][2]+1
+
+
+
+#print getVideosR2(3)
+
