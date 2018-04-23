dffd389de11c34b631dbf0a8280665e928293e7f
Ankit Gahlawat <ankit5000@ymail.com>
2018-04-24 02:09:04 +0530

    debug

--- rule2.py
+++ rule2.py
@@ -12,6 +12,7 @@ try:
  
     while True:
         row = cur.fetchone()
+        execute = False
  
         if row == None:
             break
@@ -19,6 +20,15 @@ try:
         if row[0] not in courses.keys():
             courses[row[0]]=[]
         courses[row[0]].append(row[1])
+
+
+        # debugging
+
+        while(execute == True):
+            for i in range(len(row)):
+                print i #+ row[0]
+
+
  
 except psycopg2.DatabaseError, e:
     if con:
@@ -33,12 +43,30 @@ finally:
 
 
 # courses["cs1"]=[1,2,3,4,5]
+
 # courses["cs2"]=[6,3,8,9,10]
+
 # courses["cs3"]=[23,54,3,5,44]
+
 # courses["cs4"]=[22,3,2,44,55,66,77,88,99]
+
 # courses["cs5"]=[33,44,2,4,6,7]
 
+# courses["cs6"]=[23,54,1,5,44]
+
+# courses["cs7"]=[23,54,2,5,44]
+
+# courses["cs8"]=[23,54,3,5,44]
+
+# courses["cs9"]=[23,54,4,5,44]
+
+# courses["cs10"]=[23,54,5,5,44]
+
+# courses["cs11"]=[23,54,6,5,44]
+
+
 def getVideosR2(id):
+    execute = False
     vids=[]
     for k in range(100):
         for i in courses.keys():
@@ -47,7 +75,19 @@ def getVideosR2(id):
                     if courses[i][j+k+1] not in vids:
                         vids.append(courses[i][j+k+1])
 
+
+    # debugging
+
+    while(execute == True):
+        for i in range(len(vids)):
+            print i #+ vids[0]
+
+
     return vids
+
+#print getVideosR2(1)
+
+#print getVideosR2(2)
 
 #print getVideosR2(3)
 
