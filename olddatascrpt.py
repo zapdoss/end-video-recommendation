38671a7075d548e8d84c16d55dfa61bffa2594b8
udjindal <jindaludit23@gmail.com>
2018-04-24 08:07:16 +0530

    Rectified training script

--- olddatascrpt.py
+++ olddatascrpt.py
@@ -1,4 +1,5 @@
 import psycopg2
+import sequential
 import sys
 import fromKenlist
 import tags
@@ -41,39 +42,49 @@ finally:
 # courses["cs5"]=[33,44,2,4,6,7]
 
 videos={}
+c=1
 for k in usermap.keys():
+    print c,
+    print len(usermap.keys()),
     print k,
     print usermap[k]
     for i in range(len(usermap[k])):
-        if usermap[k][i] not in videos.keys():
-            videos[usermap[k][i]]=[5,3,0]
-        mod=0
-        #course rule
-        similarVideos=fromKenlist.fromKenlist(usermap[k][i])
-        if(i<len(usermap[k])-1):
-            if(usermap[k][i+1] in similarVideos):
-                videos[usermap[k][i]][0]=videos[usermap[k][i]][0]+1
-                mod=1
-        if(i<len(usermap[k])-2):
-            if(usermap[k][i+2] in similarVideos):
-                videos[usermap[k][i]][0]=videos[usermap[k][i]][0]+1
-                mod=1
-        #tags
-        similarVideos=tags.tags(usermap[k][i])
-        if(i<len(usermap[k])-1):
-            if(usermap[k][i+1] in similarVideos):
-                videos[usermap[k][i]][1]=videos[usermap[k][i]][1]+1
-                mod=1
-        if(i<len(usermap[k])-2):
-            if(usermap[k][i+2] in similarVideos):
-                videos[usermap[k][i]][1]=videos[usermap[k][i]][1]+1
-                mod=1
-        #similarusers
-        if mod==0:
-            videos[usermap[k][i]][2]=videos[usermap[k][i]][2]+1
+        try:
+            if usermap[k][i] not in videos.keys():
+                videos[usermap[k][i]]=[5,3,0]
+            mod=0
+            #course rule
+            similarVideos=fromKenlist.fromKenlist(usermap[k][i])
+            if(i<len(usermap[k])-1):
+                if(usermap[k][i+1] in similarVideos):
+                    videos[usermap[k][i]][0]=videos[usermap[k][i]][0]+1
+                    mod=1
+            if(i<len(usermap[k])-2):
+                if(usermap[k][i+2] in similarVideos):
+                    videos[usermap[k][i]][0]=videos[usermap[k][i]][0]+1
+                    mod=1
+            #tags
+            similarVideos=tags.tags(usermap[k][i])
+            if(i<len(usermap[k])-1):
+                if(usermap[k][i+1] in similarVideos):
+                    videos[usermap[k][i]][1]=videos[usermap[k][i]][1]+1
+                    mod=1
+            if(i<len(usermap[k])-2):
+                if(usermap[k][i+2] in similarVideos):
+                    videos[usermap[k][i]][1]=videos[usermap[k][i]][1]+1
+                    mod=1
+            #similarusers
+            if mod==0:
+                similarVideos=sequential.sequential(usermap[k][i])
+                if(i<len(usermap[k])-1):
+                    if(usermap[k][i+1] in similarVideos):
+                        videos[usermap[k][i]][2]=videos[usermap[k][i]][2]+1
 
-        print usermap[k][i],
-        print videos[usermap[k][i]]
+            print usermap[k][i],
+            print videos[usermap[k][i]]
+        except:
+            pass
+    c=c+1
 
 print videos
 
